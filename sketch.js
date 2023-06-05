3911fe72dda3e3a4bcd0807d38438443e6a376e8
devshah21 <mail2devshah@gmail.com>
2023-06-05 08:56:49 -0400

    updated js for nose detection

--- sketch.js
+++ sketch.js
@@ -1,11 +1,26 @@
 let capture;
 let posenet;
+let noseX, noseY;
+let singlePose;
 
 function setup() {
     createCanvas(800, 500);
     capture = createCapture(VIDEO)
     capture.hide()
     posenet = ml5.poseNet(capture, modelLoaded);
+    posenet.on('pose', receivedPoses)
+}
+
+function receivedPoses(poses) { 
+    console.log(poses);
+
+    if(poses.length > 0){
+        singlePose = poses[0].pose;
+        noseX = singlePose.nose.x;
+        noseY = singlePose.nose.y;
+    }
+
+    console.log(noseX + " " + noseY);
 }
 
 function modelLoaded() {
@@ -14,5 +29,7 @@ function modelLoaded() {
 
 function draw() {
     image(capture, 0, 0, 800, 600);
+    fill(255, 0, 0)
+    ellipse(noseX, noseY, 30, 30)
 }
 
