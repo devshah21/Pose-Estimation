9cdc7fcf809e216836abdaf39da6aae464e54dcb
devshah21 <mail2devshah@gmail.com>
2023-06-05 08:47:46 -0400

    made pose estimation model

--- sketch.js
+++ sketch.js
@@ -1,9 +1,16 @@
 let capture;
+let posenet;
 
 function setup() {
     createCanvas(800, 500);
     capture = createCapture(VIDEO)
+    capture.hide()
+    posenet = ml5.poseNet(capture, modelLoaded);
 }
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+  }
 
 function draw() {
     image(capture, 0, 0, 800, 600);
