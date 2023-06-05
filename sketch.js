06edcd6ef5dc36e7a6415b097f90de02bb88ba7a
devshah21 <mail2devshah@gmail.com>
2023-06-05 09:08:31 -0400

    added full functionality of pose estimation

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 let capture;
 let posenet;
-let noseX, noseY;
-let singlePose;
+let noseX,noseY;
+let reyeX,reyeY;
+let leyeX,leyeY;
+let singlePose,skeleton;
 
 function setup() {
     createCanvas(800, 500);
@@ -16,11 +18,8 @@ function receivedPoses(poses) {
 
     if(poses.length > 0){
         singlePose = poses[0].pose;
-        noseX = singlePose.nose.x;
-        noseY = singlePose.nose.y;
+        skeleton = poses[0].skeleton;
     }
-
-    console.log(noseX + " " + noseY);
 }
 
 function modelLoaded() {
@@ -30,6 +29,19 @@ function modelLoaded() {
 function draw() {
     image(capture, 0, 0, 800, 600);
     fill(255, 0, 0)
-    ellipse(noseX, noseY, 30, 30)
+
+    if(singlePose){
+        for(let i = 0; i < singlePose.keypoints.length; i++){
+            ellipse(singlePose.keypoints[i].position.x, singlePose.keypoints[i].position.y, 20)
+        }
+
+        stroke(255,255,255);
+        strokeWeight(5);
+        for(let j=0; j<skeleton.length; j++){
+            line(skeleton[j][0].position.x, skeleton[j][0].position.y, skeleton[j][1].position.x, skeleton[j][1].position.y)
+        }
+    }
+
+
 }
 
